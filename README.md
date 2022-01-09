6c648e2ee53bcf945eb424ef010b3603cd3881ea
nakabayashi kotaro <123windam@gmail.com>
2022-01-09 22:45:36 +0900

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+
+環境構築参考
+
+https://qiita.com/EBIHARA_kenji/items/31b7c1c62426bdabd263
---
+++ dist/index.js
@@ -0,0 +1,67 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./lib/index.ts":
+/*!**********************!*\
+  !*** ./lib/index.ts ***!
+  \**********************/
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst user_1 = __webpack_require__(/*! ./src/user */ \"./lib/src/user.ts\");\nconst user = new user_1.User('海老原', '賢次', 44);\nconst contentsElem = document.getElementById('contents');\nif (!!contentsElem) {\n    contentsElem.innerText = `${user.familyName} ${user.givenName}`;\n}\n\n\n//# sourceURL=webpack://lib_test/./lib/index.ts?");
+
+/***/ }),
+
+/***/ "./lib/src/user.ts":
+/*!*************************!*\
+  !*** ./lib/src/user.ts ***!
+  \*************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nexports.User = void 0;\nclass User {\n    constructor(familyName, givenName, age) {\n        this.age = age;\n        this.familyName = familyName;\n        this.givenName = givenName;\n    }\n}\nexports.User = User;\n\n\n//# sourceURL=webpack://lib_test/./lib/src/user.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./lib/index.ts");
+/******/ 	
+/******/ })()
+;
